b27280688f8d096f008836f36819997670862eea
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2025-10-06 10:37:02 +0200

    style: fixing errors

--- app/cafe.py
+++ app/cafe.py
@@ -14,10 +14,10 @@ class Cafe:
     def visit_cafe(self, visitor: dict) -> str:
         current_date = datetime.date.today()
         if "vaccine" not in visitor:
-            raise NotVaccinatedError("NotVaccinatedError")
+            raise NotVaccinatedError("Visitor is not vaccinated.")
         if visitor["vaccine"]["expiration_date"] < current_date:
-            raise OutdatedVaccineError("OutdatedVaccineError")
+            raise OutdatedVaccineError("expired vaccine")
         if not visitor.get("wearing_a_mask"):
-            raise NotWearingMaskError("NotWearingMaskError")
+            raise NotWearingMaskError("Need a wearing mask")
 
         return f"Welcome to {self.name}"
--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from typing import Any, List, Dict
 
 from app.cafe import Cafe
-from app.errors import VaccineError, NotWearingMaskError
+from app.errors import (
+    VaccineError,
+    NotWearingMaskError
+)
 
 
 def go_to_cafe(friends: List[Dict[str, Any]], cafe: Cafe,) -> str:
@@ -13,6 +16,6 @@ def go_to_cafe(friends: List[Dict[str, Any]], cafe: Cafe,) -> str:
             return "All friends should be vaccinated"
         except NotWearingMaskError:
             mask_to_buy += 1
-    if mask_to_buy > 0:
+    if mask_to_buy:
         return f"Friends should buy {mask_to_buy} masks"
     return f"Friends can go to {cafe.name}"
